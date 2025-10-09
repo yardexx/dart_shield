c5d1459eac5c4aaf3c7f5ba70403f38ae8a87fa2
Jaroslav Novotný <62177414+yardexx@users.noreply.github.com>
2025-10-09 08:31:03 +0200

    refactor: add explaining comment

--- lib/src/security_analyzer/report/reporters/issue_reporter.dart
+++ lib/src/security_analyzer/report/reporters/issue_reporter.dart
@@ -1,5 +1,6 @@
 import 'package:dart_shield/src/security_analyzer/report/report.dart';
 
+// Will be extended in the future.
 // ignore: one_member_abstracts
 abstract class IssueReporter {
   String report(ProjectReport report);
