7deb1078236003883f9cdc7bf1e08ac1ccecc96e
Jaroslav Novotný <62177414+yardexx@users.noreply.github.com>
2025-12-23 18:33:40 +0100

    feat!: replace gitbook with docs.page (#19)
    * feat!: replace gitbook with docs.page
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure
    
    * docs: basic structure

--- lib/src/analyzers/code/rules/rules.dart
+++ lib/src/analyzers/code/rules/rules.dart
@@ -1,9 +1,9 @@
 import 'package:analyzer/analysis_rule/analysis_rule.dart';
-import 'package:dart_shield/src/analyzers/code/rules/avoid_harcoded_urls.dart';
-import 'package:dart_shield/src/analyzers/code/rules/avoid_hardcoded_secrets.dart';
-import 'package:dart_shield/src/analyzers/code/rules/avoid_weak_hashing.dart';
-import 'package:dart_shield/src/analyzers/code/rules/prefer_https_over_http.dart';
-import 'package:dart_shield/src/analyzers/code/rules/prefer_secure_random.dart';
+import 'package:dart_shield/src/analyzers/code/rules/cryptography/avoid_weak_hashing.dart';
+import 'package:dart_shield/src/analyzers/code/rules/cryptography/prefer_secure_random.dart';
+import 'package:dart_shield/src/analyzers/code/rules/network/avoid_harcoded_urls.dart';
+import 'package:dart_shield/src/analyzers/code/rules/network/prefer_https_over_http.dart';
+import 'package:dart_shield/src/analyzers/code/rules/secrets/avoid_hardcoded_secrets.dart';
 
 final List<AnalysisRule> rules = [
   AvoidHardcodedSecrets(),
